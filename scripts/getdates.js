142efe7a39ad5ac39f7c725d707964036d009153
randiumphrey <randiumphrey@byui.edu>
2024-04-26 09:42:09 -0600

    Finished getDates JS for W1.

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,11 @@
 const dateText = document.lastModified;
 
+/*Convert the lastModified property into a date object*/
 const date = new Date(document.lastModified);
 
 const year = date.getFullYear();
+
+/* Updating html footer with current date and time. */
+document.getElementById("copyright-date").innerHTML = "&copy; " + year + " Randi Umphrey Utah";
+
+document.getElementById("lastModified").innerHTML = dateText;

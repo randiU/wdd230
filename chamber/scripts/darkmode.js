d17ebc0b313b7c45a31ef4cac8fa82e2d6fb091e
randiumphrey <randiumphrey@byui.edu>
2024-05-25 12:43:51 -0600

    Created dark mode.

---
+++ chamber/scripts/darkmode.js
@@ -0,0 +1,54 @@
+const checkbox = document.getElementById("dark-mode-switch");
+const body = document.querySelector('body');
+const text = document.getElementsByTagName('p');
+const mainHeader = document.getElementsByTagName('h1');
+const nav = document.getElementsByTagName('nav');
+const navItems = document.getElementsByTagName('a');
+const secondaryHeaders = document.getElementsByTagName('h2');
+const thirdHeaders = document.getElementsByTagName('h3');
+
+checkbox.addEventListener('change', function() {
+    if (this.checked) {
+        console.log("checked");
+        body.style.background = "#000000";
+        console.log(text);
+        for (var i = 0; i < text.length; i++) {
+            text[i].style.color = "#FFF8DC";
+        }
+
+        for (var i = 0; i < mainHeader.length; i++) {
+            mainHeader[i].style.color = "#FFF8DC";
+        }
+
+        nav[0].style.background = "#000000";
+        for (var i = 0; i < navItems.length; i++) {
+            navItems[i].style.color = "#FFF8DC";
+        }
+
+        for (var i = 0; i < thirdHeaders.length; i++) {
+            thirdHeaders[i].style.color = "#FFF8DC";
+        }
+
+    } else {
+        console.log('not checked');
+        body.style.background = "#FFF8DC";
+
+        for (var i = 0; i < text.length; i++) {
+            text[i].style.color = "#000000";
+        }
+
+        for (var i = 0; i < mainHeader.length; i++) {
+            mainHeader[i].style.color = "#6C0345";
+        }
+
+        nav[0].style.background = "#FFF8DC";
+        
+        for (var i = 0; i < navItems.length; i++) {
+            navItems[i].style.color = "#6C0345";
+        }
+
+        for (var i = 0; i < thirdHeaders.length; i++) {
+            thirdHeaders[i].style.color = "#000000";
+        }
+    }
+});

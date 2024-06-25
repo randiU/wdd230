701625eb9359da7a056ca15b03455d3baf0bcfbe
randiumphrey <randiumphrey@byui.edu>
2024-06-24 22:31:49 -0600

    Added weather assignment for lesson10

---
+++ lesson10/scripts/weather.js
@@ -0,0 +1,40 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=d299ed7d6a6b5236a4abea2f880a7218&units=imperial';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${Math.trunc(data.main.temp)}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/wn/01d@2x.png`;
+    let desc = data.weather[0].description;
+    //Capitalize each word
+    const words = desc.split(" ");
+    for (let i = 0; i < words.length; i++) {
+        words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+    }
+    desc = words.join(" ");
+
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', `${desc} icon`);
+    captionDesc.textContent = `${desc}`;
+}
